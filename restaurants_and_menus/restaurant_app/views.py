8004817087ff5a32704c5fb8098e07bf1ac349d0
Mostafa Rayhan <mostafarayhan57@gmail.com>
2024-01-25 02:12:58 +0600

    views

--- restaurants_and_menus/restaurant_app/views.py
+++ restaurants_and_menus/restaurant_app/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+# restaurant_app/views.py
 
-# Create your views here.
+from rest_framework import generics
+from .models import Restaurant, Menu, MenuItem, Order
+from .serializers import RestaurantSerializer, MenuSerializer, MenuItemSerializer, OrderSerializer
+
+class RestaurantListCreateView(generics.ListCreateAPIView):
+    serializer_class = RestaurantSerializer
+
+    def get_queryset(self):
+        return Restaurant.objects.filter(owner=self.request.user)
+
+class RestaurantDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = RestaurantSerializer
+
+    def get_queryset(self):
+        return Restaurant.objects.filter(owner=self.request.user)
+
+class MenuListCreateView(generics.ListCreateAPIView):
+    serializer_class = MenuSerializer
+
+    def get_queryset(self):
+        return Menu.objects.filter(restaurant__owner=self.request.user)
+
+class MenuDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = MenuSerializer
+
+    def get_queryset(self):
+        return Menu.objects.filter(restaurant__owner=self.request.user)
+
+class MenuItemListCreateView(generics.ListCreateAPIView):
+    serializer_class = MenuItemSerializer
+
+    def get_queryset(self):
+        return MenuItem.objects.filter(menu__restaurant__owner=self.request.user)
+
+class MenuItemDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = MenuItemSerializer
+
+    def get_queryset(self):
+        return MenuItem.objects.filter(menu__restaurant__owner=self.request.user)
+
+class OrderListCreateView(generics.ListCreateAPIView):
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
+class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
